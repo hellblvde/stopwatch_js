8a0a6260b2c07a0ac014f2bd9dcfebf488fb9263
Ruslan Yakovlev <68195302+hellblvde@users.noreply.github.com>
2024-08-30 17:51:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    let seconds = 0;
+    let tens = 0;
+    let appendSeconds = document.getElementById("seconds");
+    let appendTens = document.getElementById("tens");
+    let startButton = document.getElementById("button-start");
+    let stopButton = document.getElementById("button-stop");
+    let resetButton = document.getElementById("button-reset");
+    let Interval;
+
+    startButton.onclick = function() {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+        
+    }
+
+    stopButton.onclick = function() {
+        clearInterval(Interval);
+    }
+
+    resetButton.onclick = function() {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+    function startTimer() {
+        tens++;
+
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+
+        if (tens > 99) {
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+          }
+    }
+}
